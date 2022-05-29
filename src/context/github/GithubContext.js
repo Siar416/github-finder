8d92ae01103209e075dab8f772157584b04fa2db
Siar Wahidi <siar.wahidi@gmail.com>
2022-05-29 16:41:37 -0400

    Added functionality to clear button but creating clear user reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -36,6 +36,9 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
   // Set Loading
   const setLoading = () =>
     dispatch({
@@ -48,6 +51,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers,
       }}
     >
       {children}
