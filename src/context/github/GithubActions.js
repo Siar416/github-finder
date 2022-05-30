7a46307d4b7976e22e5b31589f00977e7c1df7d0
Siar Wahidi <siar.wahidi@gmail.com>
2022-05-30 14:54:34 -0400

    Cleaned uo actions file and installed axios. Refactored function to get user and repos to a single function instead

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,13 @@
+import axios from "axios";
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `${GITHUB_TOKEN}`,
+  },
+});
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,48 +15,15 @@ export const searchUsers = async (text) => {
     q: text,
   });
 
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `${GITHUB_TOKEN}`,
-    },
-  });
-
-  const { items } = await response.json();
-
-  return items;
+  const response = await github.get(`/search/users?${params}`);
+  return response.data.items;
 };
 
-// Get single user
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `${GITHUB_TOKEN}`,
-    },
-  });
-
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-
-    return data;
-  }
-};
-
-// Get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  });
-
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `${GITHUB_TOKEN}`,
-    },
-  });
-
-  const data = await response.json();
-
-  return data;
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
+  return { user: user.data, repos: repos.data };
 };
